ef833ad6ff9fa60046d3b363ad876caa169767c6
Yohannes <yohanness1621@gmail.com>
2026-05-03 08:30:00 +0300

    Create animation components barrel export

---
+++ components/animations/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Animation Components Barrel Export
+ * Centralized exports for all animation components and wrappers.
+ */
+
+export { Card3D } from "./Card3D";
+export { MagneticButton } from "./MagneticButton";
+export { ScrollProgress } from "./ScrollProgress";
+export { RevealSection } from "./RevealSection";
+export { PageTransition } from "./PageTransition";
+export { FloatingShapes } from "./FloatingShapes";
+export { ParallaxHero, ParallaxLayer } from "./ParallaxHero";
+export { AnimatedText } from "./AnimatedText";
