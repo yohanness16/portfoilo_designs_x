1b43620ba303b599b6288a53d275f528b2940df4
Yohannes <yohanness1621@gmail.com>
2026-05-04 09:15:00 +0300

    Create section components barrel export

---
+++ components/sections/index.ts
@@ -0,0 +1,7 @@
+export { Header } from "./Header";
+export { HeroSection } from "./HeroSection";
+export { AboutSection } from "./AboutSection";
+export { ProjectsSection } from "./ProjectsSection";
+export { SkillsSection } from "./SkillsSection";
+export { ContactSection } from "./ContactSection";
+export { Footer } from "./Footer";
