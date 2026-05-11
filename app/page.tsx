ef0402ac169be6aa195312a6ae912b5345bb1df5
Yohannes <yohanness1621@gmail.com>
2026-05-11 13:00:00 +0300

    Refactor page.tsx with semantic main element and section order

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,19 @@ import { ProjectsSection } from "@/components/sections/ProjectsSection";
 import { SkillsSection } from "@/components/sections/SkillsSection";
 import { ContactSection } from "@/components/sections/ContactSection";
 import { Footer } from "@/components/sections/Footer";
+import { FloatingShapes } from "@/components/animations/FloatingShapes";
 
 export default function Home() {
   return (
-    <>
+    <main className="relative min-h-screen bg-[#0a0a0f]">
+      <FloatingShapes />
       <Header />
-      <main className="flex-1">
-        <HeroSection />
-        <AboutSection />
-        <ProjectsSection />
-        <SkillsSection />
-        <ContactSection />
-      </main>
+      <HeroSection />
+      <AboutSection />
+      <ProjectsSection />
+      <SkillsSection />
+      <ContactSection />
       <Footer />
-    </>
+    </main>
   );
 }
