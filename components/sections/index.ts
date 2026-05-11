ba6dbef180d35e103175f67520cd04928c8a7a18
Yohannes <yohanness1621@gmail.com>
2026-05-11 17:00:00 +0300

    Add typed JSDoc header to sections barrel export

--- components/sections/index.ts
+++ components/sections/index.ts
@@ -1,7 +1,12 @@
-export { Header } from "./Header";
-export { HeroSection } from "./HeroSection";
+/**
+ * Section components barrel export.
+ * Centralized exports for all page section components.
+ */
+
 export { AboutSection } from "./AboutSection";
-export { ProjectsSection } from "./ProjectsSection";
-export { SkillsSection } from "./SkillsSection";
 export { ContactSection } from "./ContactSection";
 export { Footer } from "./Footer";
+export { Header } from "./Header";
+export { HeroSection } from "./HeroSection";
+export { ProjectsSection } from "./ProjectsSection";
+export { SkillsSection } from "./SkillsSection";
